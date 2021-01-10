5a0403412afd1d6a441105527e813619163c4383
peret <daniel.perretz@hujl.mail.ac.il>
2021-01-11 01:28:37 +0200

    בדיקה של כל הקורדינטות ואם הן נמצאות בלוח בדיקה אם קורדינטות הן סמוכות בדיקה אם יש קורדינטה פעמיים

--- ex12_utils.py
+++ ex12_utils.py
@@ -8,12 +8,25 @@ def load_words_dict(file_path):
 
 print(load_words_dict("boggle_dict.txt"))
 
+
 def is_valid_path(board, path, words):
     for coor in path:
         x, y = coor
         if x > MAX_ROW or y > MAX_COL:
             return None
+    for i in range(len(path)-1):
+        x1,y1 = path[i]
+        x2,y2 = path[i+1]
+        if abs(x1-x2) > 1 or abs(y1-y2) > 1:
+            return None
+            break
+    dup_list = list(set(path)) # check for double coordinate
+    if len(dup_list) < len(path):
+        return None
 
+    return "Word_Test"
+
+print(is_valid_path([],[(1, 2) ,(2, 3),(1, 3)],"noy"))
 
 
 def find_length_n_words(n, board, words):
