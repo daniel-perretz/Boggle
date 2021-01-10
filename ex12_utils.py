56f746d373cb3d768fae53e562becb14f49a013a
peret <daniel.perretz@hujl.mail.ac.il>
2021-01-10 21:00:04 +0200

    sadfasf

--- ex12_utils.py
+++ ex12_utils.py
@@ -1,6 +1,10 @@
 def load_words_dict(file_path):
-    pass
+    with open(file_path) as f:
+        content = f.read().splitlines()
+        word_dict = {x: True for x in content}
+    return word_dict
 
+print(load_words_dict("boggle_dict.txt"))
 
 def is_valid_path(board, path, words):
     pass
