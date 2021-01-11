34d02e28153cd8fedf0043682b01d0f1d65ee11f
peret <daniel.perretz@hujl.mail.ac.il>
2021-01-11 02:20:35 +0200

    בדיקה של כל הקורדינטות ואם הן נמצאות בלוח בדיקה אם קורדינטות הן סמוכות בדיקה אם יש קורדינטה פעמיים עדכון: חושב שהבדיקה אם המילה נמצאת, עובדת.

--- ex12_utils.py
+++ ex12_utils.py
@@ -6,8 +6,6 @@ def load_words_dict(file_path):
         word_dict = {x: True for x in content}
     return word_dict
 
-print(load_words_dict("boggle_dict.txt"))
-
 
 def is_valid_path(board, path, words):
     for coor in path:
@@ -23,11 +21,42 @@ def is_valid_path(board, path, words):
     dup_list = list(set(path)) # check for double coordinate
     if len(dup_list) < len(path):
         return None
-
-    return "Word_Test"
-
-print(is_valid_path([],[(1, 2) ,(2, 3),(1, 3)],"test"))
+    word = ''
+    for coor in path:
+        x, y = coor
+        word = word + board[x][y]
+    if word in words:
+        return word
+    return None
 
 
 def find_length_n_words(n, board, words):
     pass
+
+def main():
+
+    board = [
+        ['A', 'E', 'A', 'N', 'E', 'G'],
+        ['A', 'H', 'S', 'P', 'C', 'O'],
+        ['A', 'S', 'P', 'F', 'F', 'K'],
+        ['O', 'B', 'J', 'O', 'A', 'B'],
+        ['I', 'O', 'T', 'M', 'U', 'C'],
+        ['R', 'Y', 'V', 'D', 'E', 'L'],
+        ['L', 'R', 'E', 'I', 'X', 'D'],
+        ['E', 'I', 'U', 'N', 'E', 'S'],
+        ['W', 'N', 'G', 'E', 'E', 'H'],
+        ['L', 'N', 'H', 'N', 'R', 'Z'],
+        ['T', 'S', 'T', 'I', 'Y', 'D'],
+        ['O', 'W', 'T', 'O', 'A', 'T'],
+        ['E', 'R', 'T', 'T', 'Y', 'L'],
+        ['T', 'O', 'E', 'S', 'S', 'I'],
+        ['T', 'E', 'R', 'W', 'H', 'V'],
+        ['N', 'U', 'I', 'H', 'M', 'QU']
+    ]
+
+    dic = load_words_dict("boggle_dict.txt")
+    print(is_valid_path(board, [(1, 2), (2, 3), (1, 3)], dic))
+
+
+if __name__ == '__main__':
+    main()
