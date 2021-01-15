80aed70691608d23b6eaa8d745fdcf92969c2e75
peret <daniel.perretz@hujl.mail.ac.il>
2021-01-15 18:13:27 +0200

    בדיקה של כל הקורדינטות ואם הן נמצאות בלוח בדיקה אם קורדינטות הן סמוכות בדיקה אם יש קורדינטה פעמיים עדכון: חושב שהבדיקה אם המילה נמצאת, עובדת.

--- Model.py
+++ Model.py
@@ -44,3 +44,56 @@ class Model:
     def get_found_words(self):
         return self.__found_words
 
+
+import tkinter as tki
+from datetime import datetime
+
+H24_MODE = 24
+H12_MODE = 12
+
+
+class ClockGUI:
+
+    def __init__(self, clock_model):
+        self._clock_model = clock_model
+        self._root = tki.Tk()
+        self._clock_display = tki.Label(font=("Courier", 30), width=11)
+        self._clock_display.pack()
+
+        # just to demo binding on the label:
+        self._clock_display.bind("<Button-1>", self._label_clicked)
+
+        self._button = tki.Button(text="mode", font=("Courier", 30))
+        self._button.pack()
+        self._button["command"] = self._clock_model.switch_mode
+
+    def _label_clicked(self, event):
+        print(event.x, event.y)
+        print(dir(event))
+
+    def _animate(self):
+        self._clock_display["text"] = self._clock_model.get_time_str()
+        self._root.after(10, self._animate)
+
+    def run(self):
+        self._animate()
+        self._root.mainloop()
+
+
+class ClockModel:
+    def __init__(self, mode=H24_MODE):
+        self._mode = mode
+
+    def switch_mode(self):
+        if self._mode is H12_MODE:
+            self._mode = H24_MODE
+        else:
+            self._mode = H12_MODE
+
+    def get_time_str(self):
+        now = datetime.now()
+        if self._mode is H24_MODE:
+            return now.strftime("%H:%M:%S:%f")[:-4]
+        else:
+            return now.strftime("%p %I:%M:%S")
+
--- boggle_gui.py
+++ boggle_gui.py
@@ -1,19 +1,57 @@
 import tkinter as tki
 from typing import Callable, Dict, List, Any
 
+NUM_COLS = 4
+NUM_ROWS = 4
+
+
 class BoggleGui:
     def __init__(self):
         self.root = tki.Tk()
-        self.title = tki.Label(text="Boggle")
-        self.start_button = tki.Button(text="START ", bg="black", fg="white")
+        self.upper_frame = tki.Frame()
+        self.lower_frame = tki.Frame()
+        self.mid_frame = tki.Frame()
+        self.title = tki.Label(text="Boggle" , master=self.lower_frame)
+        self.start_button = tki.Button(text="START ", bg="red", fg="white",master=self.upper_frame)
+        self.buttons_dic = []
+        for i in range(NUM_COLS):
+            for j in range(NUM_ROWS):
+                frame = tki.Frame(
+                    master=self.mid_frame,
+                    relief=tki.RAISED,
+                )
+                frame.grid(row=i, column=j)
+                button = tki.Button(master=frame)
+                self.buttons_dic.append(button)
+                button.pack()
+        self.pack()
+
+    def set_button_text(self, board):
+        count = 0
+        for row in board:
+            for col in row:
+                self.buttons_dic[count]["text"] = board[row][col]
+                count += 1
+
+    def pack(self):
+        self.upper_frame.pack(side=tki.TOP)
+        self.mid_frame.pack()
+        self.lower_frame.pack(side=tki.BOTTOM)
         self.start_button.pack()
         self.title.pack()
 
 
+    def create_buttons(self):
+        for i in range(NUM_COLS):
+            tki.Grid.columnconfigure(self._lower_frame, i, weight=1)  # type: ignore
+
+        for i in range(NUM_ROWS):
+            tki.Grid.rowconfigure(self._lower_frame, i, weight=1)
+
     def run(self) -> None:
         self.root.mainloop()
+
 
 if __name__ == "__main__":
     cg = BoggleGui()
     cg.run()
-
