c91ded78e4a8ff2dd776b2ff83450d2c311face3
tamirh <tamir.herzberg@mail.huji.ac.il>
2021-01-15 17:13:09 +0200

    gui

--- boggle_gui.py
+++ boggle_gui.py
@@ -3,14 +3,15 @@ from typing import Callable, Dict, List, Any
 
 class BoggleGui:
     def __init__(self):
-        root = tki.Tk()
-        self._main_window = root
-        self._display_label = tki.Label(self._outer_frame, font=("Courier", 30),
-                                        bg=REGULAR_COLOR, width=23, relief="ridge")
-        self._display_label.pack(side=tki.TOP, fill=tki.BOTH)
+        self.root = tki.Tk()
+        self.title = tki.Label(text="Boggle")
+        self.start_button = tki.Button(text="START", bg="black", fg="white")
+        self.start_button.pack()
+        self.title.pack()
+
 
     def run(self) -> None:
-        self._main_window.mainloop()
+        self.root.mainloop()
 
 if __name__ == "__main__":
     cg = BoggleGui()
